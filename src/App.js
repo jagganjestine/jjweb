0cbe790d995c0dff369914e8264cedf4e20e195b
Jaggan Jestine <78127002+jaj180010@users.noreply.github.com>
2023-04-17 18:10:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,17 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import { Link, Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <nav className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route index element={<Home />} path="/" />
+        <Route index element={<About />} path="/about" />
+        <Route element={<Projects />} path="/projects" />
+        <Route element={<Contact />} path="/contact" />
       </Routes>
       <Footer />
     </div>
