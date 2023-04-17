477eb2394d74959e8a03f7929c9d49072af1df65
Jaggan Jestine <78127002+jaj180010@users.noreply.github.com>
2023-04-17 18:06:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route index element={<Home />} path="/" />
-        <Route index element={<About />} path="/about" />
-        <Route element={<Projects />} path="/projects" />
-        <Route element={<Contact />} path="/contact" />
-      </Routes>
-      <Footer />
+      <Router>
+        <Header />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
