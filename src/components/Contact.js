24a82656d59e7a8014d328abe29350890c1ebfac
Jaggan Jestine <78127002+jaj180010@users.noreply.github.com>
2023-04-17 23:10:10 -0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,40 +3,52 @@ import emailjs from 'emailjs-com';
 import './Contact.css';
 
 const Contact = () => {
-  const [formValues, setFormValues] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
-
-  const handleChange = (event) => {
-    setFormValues({ ...formValues, [event.target.name]: event.target.value });
-  };
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const [statusMessage, setStatusMessage] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    emailjs.send(
-      'service_57pikjr',
-      'template_niwjbgg',
-      formValues,
-      'mtoqmZ96KmrU6quxJ',
-    )
-      .then((result) => {
-        alert('Message sent, thank you for contacting me.');
-      }, (error) => {
-        alert('An error occurred, please try again later.');
-      });
+    if (!name || !email || !subject || !message) {
+      setStatusMessage('Please fill out all fields.');
+      return;
+    }
 
-    setFormValues({
-      name: '',
-      email: '',
-      message: '',
-    });
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: 'Jaggan',
+      subject: subject,
+      message_html: message,
+    };
+
+    emailjs
+      .send(
+        'service_ki03vjd',
+        'template_w1mjsf8',
+        templateParams,
+        'user_Mdmbtt2O5r0Ro5J2Gkw1c'
+      )
+      .then(
+        (response) => {
+          setStatusMessage('Your message has been sent!');
+          setName('');
+          setEmail('');
+          setSubject('');
+          setMessage('');
+        },
+        (error) => {
+          console.log('FAILED...', error);
+          setStatusMessage('Failed to send message. Please try again later.');
+        }
+      );
   };
 
   return (
-    <section id="contact" className="container my-5">
+    <section id="contact" className="container">
       <h2>Contact Me</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
@@ -45,22 +57,29 @@ const Contact = () => {
             type="text"
             className="form-control"
             id="name"
-            name="name"
-            value={formValues.name}
-            onChange={handleChange}
-            required
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
         </div>
         <div className="form-group">
-          <label htmlFor="email">Email</label>
+          <label htmlFor="email">Email address</label>
           <input
             type="email"
             className="form-control"
             id="email"
-            name="email"
-            value={formValues.email}
-            onChange={handleChange}
-            required
+            aria-describedby="emailHelp"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="subject">Subject</label>
+          <input
+            type="text"
+            className="form-control"
+            id="subject"
+            value={subject}
+            onChange={(event) => setSubject(event.target.value)}
           />
         </div>
         <div className="form-group">
@@ -68,16 +87,15 @@ const Contact = () => {
           <textarea
             className="form-control"
             id="message"
-            name="message"
             rows="5"
-            value={formValues.message}
-            onChange={handleChange}
-            required
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
           ></textarea>
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+        {statusMessage && <p className="status-message">{statusMessage}</p>}
       </form>
     </section>
   );
