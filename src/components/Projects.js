ed32f0b08adab389b354a169ee8a8db4d4d0c584
Jaggan Jestine <78127002+jaj180010@users.noreply.github.com>
2023-04-28 15:05:15 -0500

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ const Projects = () => {
       <div className="projects-container">
     <div className="project-card">
   <img src={myPhoto} alt="Git" className="card-img-top" />
-  <div className="card-body">
+  <div className="card-body card-padding">
     <h5 className="card-title">Personal Website</h5>
     <p className="card-text">A React-based personal website showcasing my experience, projects, and contact information. </p>
     <div className="language-dot"><span className="dot dot-js"></span><p>JavaScript</p></div>
@@ -18,7 +18,7 @@ const Projects = () => {
 </div>    
 <div className="project-card">
         <img src={myPhoto} alt="Git" className="card-img-top" />
-          <div className="card-body">
+          <div className="card-body card-padding">
             <h5 className="card-title">Stock Portfolio Optimization</h5>
             <p className="card-text">A machine learning project that predicts stock market trends using historical data and sentiment analysis to optimize your porfolio.</p>
             <div className="language-dot"><span className="dot dot-python"></span><p>Python</p></div>
@@ -28,7 +28,7 @@ const Projects = () => {
         <div className="project-card">
         <img src={myPhoto} alt="Git" className="card-img-top" />
           <div className="card-body text-center">
-            <h5 className="card-title">Text Editor</h5>
+            <h5 className="card-title card-padding">Text Editor</h5>
             <p className="card-text">A cross-platform desktop text editor with support for syntax highlighting and collaborative editing.</p>
             <div className="language-dot"><span className="dot dot-java"></span><p>Java</p></div>
             <a href="https://github.com/jagganjestine/text-editor" className="btn btn-github" target="_blank" rel="noreferrer"> View on GitHub</a>
@@ -36,7 +36,7 @@ const Projects = () => {
         </div>
         <div className="project-card">
         <img src={myPhoto} alt="Git" className="card-img-top" />
-           <div className="card-body">
+           <div className="card-body card-padding">
            <h5 className="card-title">Calendar</h5>
             <p className="card-text">A simple calendar that helps users manage their schedules and events.</p>
             <div className="language-dot"><span className="dot dot-java"></span><p>Java</p></div>
@@ -45,7 +45,7 @@ const Projects = () => {
         </div>  
         <div className="project-card">
         <img src={myPhoto} alt="Git" className="card-img-top" />
-          <div className="card-body">
+          <div className="card-body card-padding">
             <h5 className="card-title">Travel Planner</h5>
             <p className="card-text">A web application that allows users to plan their trips, find attractions, and share itineraries with others.</p>
             <div className="language-dot"><span className="dot"></span><p>C</p></div>
@@ -55,7 +55,7 @@ const Projects = () => {
   
         <div className="project-card">
         <img src={myPhoto} alt="Git" className="card-img-top" />
-          <div className="card-body">
+          <div className="card-body card-padding">
             <h5 className="card-title">Language Learner</h5>
             <p className="card-text">A language learning platform with interactive lessons, quizzes, and a chatbot for practicing conversations.</p>
             <div className="language-dot"><span className="dot"></span><p>C</p></div>
